b9f1b7cca8dde5d9622ae9b08f69086f4d07dbc4
Guilherme Amadio <amadio@cern.ch>
2022-02-17 11:01:43 +0100

    Update report.sh to use new awk scripts

--- perf/scripts/report.sh
+++ perf/scripts/report.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-# add directory where this script is located to AWKPATH, PATH
+# add directory where this script is located to PATH, AWKPATH
 export AWKPATH=$(realpath $(dirname "${BASH_SOURCE[0]}"))
-export PATH=$(realpath $(dirname "${BASH_SOURCE[0]}")):$PATH
+export PATH=$AWKPATH:$PATH
 
 for TEST in $@; do
-	[[ -f ${TEST}.perf ]] && flamegraph.sh ${TEST}.perf
-	[[ -d ${TEST}-treemap ]] && treemap.sh ${TEST}.perf ${TEST}-treemap/treemap.json
-
-	for M in core L1; do
+	for M in cpu cache; do
 		if [[ ${TEST}-${M}.perf -nt ${TEST}-${M}.txt ]]; then
 			[[ -f ${TEST}-${M}.txt ]] && mv ${TEST}-${M}.txt{,.old}
-			perf report --header --percent-limit 0 \
+			perf report --stdio --header --percent-limit 0 \
 				-F period,dso,symbol -i ${TEST}-${M}.perf >| ${TEST}-${M}.txt
 		fi
 
@@ -20,7 +17,10 @@ for TEST in $@; do
 		fi
 	done
 
-	for M in core L1; do
+	for M in cpu cache; do
 		awk -f ${M}.awk ${TEST}-${M}.txt
 	done
+
+	[[ -f ${TEST}.perf ]] && flamegraph.sh ${TEST}.perf
+	[[ -d ${TEST}-treemap ]] && treemap.sh ${TEST}.perf ${TEST}-treemap/treemap.json
 done
