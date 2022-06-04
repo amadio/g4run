5a8a3b973852a87c920efd5023ae53a79ebfe23f
Harshil Jani <harshiljani2002@gmail.com>
2022-06-05 04:00:32 +0530

    Made Responsive and made code clean with comments and formatting

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+//Logic for Switching the visualization tabs without reloading the page
 const openTab = (elem, tabName) => {
-    console.log(elem)
     const tabs = document.getElementsByClassName("visual-tabs");
     for (let i = 0; i < tabs.length; i++) {
         tabs[i].classList.remove("active");
@@ -14,21 +14,6 @@ const openTab = (elem, tabName) => {
 
     elem.classList.add("active")
 }
+
+// By default have a tab selected initially
 document.getElementById("defaultOpenTab").click();
-
-const openReport = (elem, reportName) => {
-    const reports = document.getElementsByClassName("report-tabs");
-    for(let i=0; i< reports.length; i++){
-        reports[i].classList.remove("active");
-    }
-    const reportContent = document.getElementsByClassName("report-contents");
-    for(let i=0 ;i< reportContent.length; i++){
-        reportContent[i].style.display = "none";
-    }
-    document.getElementById(reportName).style.display = "block";
-    elem.classList.add("active");
-}
-document.getElementById("defaultOpenReport").click();
-
-const reader = new FileReader();
-console.log(reader);
