98584ea211fcf41558a430cd83fb1493cff3fdf9
Harshil Jani <harshiljani2002@gmail.com>
2022-06-15 19:40:00 +0530

    Selecting Multiple Data files and updating the Page

--- Scripts/csv_to_table.js
+++ Scripts/csv_to_table.js
@@ -1,13 +1,21 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
-// Selection Fields
+let csv_report = "demo.csv";
+
+// Getting the Options for selecting the report i.e. CSV File
+const report_selection = () => {
+    d3.select("#report-selection").append("option").text("demo.csv");
+    d3.select("#report-selection").append("option").text("demo2.csv");
+}
+
+// Selection Fields for metrics
 const name_fields = table_columns => {
     for (let i = 0; i < table_columns.length - 1; i++) {
-        d3.select("select").append("option").text(table_columns[i]);
+        d3.select("#column_fields").append("option").text(table_columns[i]);
     }
 }
 
-
+// Convert the CSV into Tables
 const tabulate = (data, table_columns) => {
     const table = d3.select("#html-table").append("table").attr("id", "report-table");
     const thead = table.append("thead")
@@ -45,8 +53,24 @@ const tabulate = (data, table_columns) => {
         });
 }
 
-d3.csv("demo.csv").then(data => {
+// Load the CSV data into HTML using d3
+const load_CSV = myVar => {
+d3.csv(`Data/${myVar}`).then(data => {
+    console.log(myVar);
     const table_columns = data.columns;
     tabulate(data, table_columns);
     name_fields(table_columns);
 });
+};
+
+// Update the page on selecting the other Data File (CSV) for generating reports
+document.getElementById("report-selection").addEventListener("change",(e) => {
+    csv_report = e.target.value;
+    document.getElementById("html-table").innerHTML = "";
+    console.log(csv_report);
+    load_CSV(csv_report);
+})
+
+// Initialize the page to be viewed by default reports
+load_CSV(csv_report);
+report_selection();
