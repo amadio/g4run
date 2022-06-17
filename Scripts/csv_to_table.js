fb4c45fd5c9ccaac17c40f5f812fc615ae4f6cf9
Harshil Jani <harshiljani2002@gmail.com>
2022-06-17 21:30:30 +0530

    Converting Filters to D3 Logic

--- Scripts/csv_to_table.js
+++ Scripts/csv_to_table.js
@@ -3,7 +3,8 @@ import * as d3 from "https://cdn.skypack.dev/d3@7";
 let csv_report = "demo.csv";
 
 // Store all the report files in this array
-const all_reports = ["demo.csv","demo2.csv"];
+const all_reports = ["demo.csv", "pythia-cache.csv"];
+
 // Getting the Options for selecting the report i.e. CSV File
 const report_selection = () => {
     all_reports.forEach(file => {
@@ -12,20 +13,55 @@ const report_selection = () => {
 }
 
 // Selection Fields for metrics
-const name_fields = table_columns => {
-    for (let i = 0; i < table_columns.length - 1; i++) {
-        d3.select("#column_fields").append("option").text(table_columns[i]);
+const name_fields = numeric_columns => {
+    const options = [];
+    for (let i = 0; i < numeric_columns.length; i++) {
+        d3.selectAll("#column_fields").append("option").text(numeric_columns[i]).attr("class", "csv-columns");
     }
+    // Removing Duplicate Entries in Dropdown
+    document.querySelectorAll(".csv-columns").forEach((option) => {
+        if (options.includes(option.value)) {
+            option.remove();
+        } else {
+            options.push(option.value);
+        }
+    })
 }
 
 // Convert the CSV into Tables
-const tabulate = (data, table_columns) => {
+const tabulate = (data, table_columns, numeric_columns) => {
     const table = d3.select("#html-table").append("table").attr("id", "report-table");
     const thead = table.append("thead")
     const tbody = table.append("tbody");
     thead.append("tr").selectAll("th").data(table_columns).enter().append("th").text(d => d);
+    const selectField = document.getElementById("column_fields").value;
 
-    const rows = tbody.selectAll("tr").data(data).enter().append("tr");
+    // Threshold Logics
+    let h_input, l_input, h_filter, l_filter;
+    h_input = document.getElementById("h_threshold");
+    l_input = document.getElementById("l_threshold");
+    h_filter = parseFloat(h_input.value);
+    l_filter = parseFloat(l_input.value);
+
+    // Count to check if there are no available entries
+    let count = 0;
+    const rows = tbody.selectAll("tr").data(data.filter(d => {
+        if (h_filter >= 0 && l_filter >= 0) {
+            if (d[selectField] >= l_filter && d[selectField] <= h_filter) {
+                count++;
+                return d;
+            } else {
+                return null;
+            }
+        }
+        return d;
+    })).enter().append("tr");
+
+    // If there are no entries then report for the same
+    if (h_filter >= 0 && l_filter >= 0 && count == 0) {
+        d3.select("table").remove();
+        d3.select("#html-table").text("No valid data available for given range")
+    }
 
     const cells = rows.selectAll('td')
         .data(row => (
@@ -49,20 +85,33 @@ const tabulate = (data, table_columns) => {
                 return d3.scaleLinear().domain([5.5, 8.5]).range(["#5225f5", "#f52540"])(parseFloat(d.value));
             if (d.column == "B_Miss")
                 return d3.scaleLinear().domain([0, 2]).range(["#5225f5", "#f52540"])(parseFloat(d.value));
+            if (d.column == "L1_icache_load_misses")
+                return d3.scaleLinear().domain([0, 520438188]).range(["#5225f5", "#f52540"])(parseFloat(d.value));
+            if (d.column == "L1_dcache_load_misses")
+                return d3.scaleLinear().domain([0, 7676726522]).range(["#5225f5", "#f52540"])(parseFloat(d.value));
+            if (d.column == "L1_dcache_loads")
+                return d3.scaleLinear().domain([0, 108521744895]).range(["#5225f5", "#f52540"])(parseFloat(d.value));
+            if (d.column == "instructions")
+                return d3.scaleLinear().domain([0, 141732036696]).range(["#5225f5", "#f52540"])(parseFloat(d.value));
         }).style("color", d => {
-            if (d.column == "Symbol")
-                return "black";
-            return "white";
+            if (numeric_columns.includes(d.column) || d.column == "B_Miss")
+                return "white";
         });
 }
 
 // Load the CSV data into HTML using d3
 const load_CSV = myVar => {
-d3.csv(`Data/${myVar}`).then(data => {
-    const table_columns = data.columns;
-    tabulate(data, table_columns);
-    name_fields(table_columns);
-});
+    d3.csv(`Data/${myVar}`).then(data => {
+        const table_columns = data.columns;
+        let numeric_columns = [];
+        table_columns.forEach(cols => {
+            if (isNaN(data[0][cols]) == false) {
+                numeric_columns.push(cols)
+            }
+        });
+        name_fields(numeric_columns);
+        tabulate(data, table_columns, numeric_columns);
+    });
 };
 
 // Download the CSV file on clicking the Button
@@ -71,12 +120,30 @@ document.getElementById("csv-download").addEventListener("click", () => {
 })
 
 // Update the page on selecting the other Data File (CSV) for generating reports
-document.getElementById("report-selection").addEventListener("change",(e) => {
+document.getElementById("report-selection").addEventListener("change", (e) => {
     csv_report = e.target.value;
     document.getElementById("html-table").innerHTML = "";
+    d3.selectAll(".csv-columns").remove();
     load_CSV(csv_report);
 })
 
 // Initialize the page to be viewed by default reports
 load_CSV(csv_report);
 report_selection();
+
+// Apply Button for Thresholds
+document.getElementById("apply-btn").addEventListener("click", () => {
+    d3.select("table").remove();
+    load_CSV(csv_report);
+})
+
+// Reset the reports without filters
+document.getElementById("reset-filter").addEventListener("click", () => {
+    const h_input = document.getElementById("h_threshold");
+    const l_input = document.getElementById("l_threshold");
+    h_input.value = "";
+    l_input.value = "";
+    d3.select("table").remove();
+    d3.select("#html-table").text("");
+    load_CSV(csv_report);
+})
