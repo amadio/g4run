d63580b78381630e639bfc7ce9bd957c0528d5da
Guilherme Amadio <amadio@cern.ch>
2022-09-05 14:36:07 +0200

    Add csv-diff.sh script
    This script takes two perf.data files and outputs a CSV file where
    each column has a suffix "_old" ("_new") for data coming from the
    first (second) run of perf record. This file can be used to create
    a table report showing performance differences for the two runs.

---
+++ perf/scripts/csv-diff.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+INPUT1="perf report -i ${1:-perf.data.old} --header --percent-limit 0 -g none -F period,comm,dso,symbol"
+INPUT2="perf report -i ${2:-perf.data} --header --percent-limit 0 -g none -F period,comm,dso,symbol"
+OUTPUT=${3:-${1%%.*}-diff.csv}
+
+awk '
+BEGIN {
+	curr_file = 0
+}
+
+BEGINFILE {
+	curr_file++
+	nevents = 1
+}
+
+/name = / {
+	sub(",", "", $0)
+	sub(":.*", "", $6)
+	if ($6 != "dummy") {
+		evname[nevents] = gensub("[^a-zA-Z0-9]", "_", "g", $6)
+		event[$6] = nevents++
+	}
+}
+
+/\[.\]/ && $event["cycles"] > 0 {
+	comm = $nevents
+	dso = $(nevents + 1)
+	symbol = gensub(".*\\] " , "", "g")
+	for (e in evname) {
+		data[comm][dso][symbol][curr_file][e] = $e
+		total[curr_file][e] += $e
+	}
+}
+
+ENDFILE {
+	delete event
+}
+
+END {
+	for(i in evname)
+		printf("%s_old,%s_new,", evname[i], evname[i]);
+	printf("comm,dso,symbol");
+	printf("\n");
+
+	for (comm in data) {
+		for (dso in data[comm]) {
+			for (symbol in data[comm][dso]) {
+				for (e in evname) {
+					e_old = data[comm][dso][symbol][1][e]
+					e_new = data[comm][dso][symbol][2][e]
+					printf("%d,%d,", e_old, e_new)
+				}
+				printf("\"%s\",\"%s\",\"%s\"\n", comm, dso, symbol);
+			}
+		}
+	}
+}
+' <(${INPUT1}) <(${INPUT2}) >${OUTPUT}
