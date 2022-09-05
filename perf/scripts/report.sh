44e75028d0cb1de554c499aed5995807705bcc0d
Guilherme Amadio <amadio@cern.ch>
2022-09-05 14:55:17 +0200

    Update perf/CMakeLists.txt and report.sh script
    Now, rather than creating the usual text report, the script
    report.sh creates the CSV data necessary for the HTML report.

--- perf/scripts/report.sh
+++ perf/scripts/report.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
+set -ex
+
 # add directory where this script is located to PATH, AWKPATH
 export AWKPATH=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 export PATH=$AWKPATH:$PATH
 
 for TEST in $@; do
-	for M in cpu cache; do
-		if [[ ${TEST}-${M}.perf -nt ${TEST}-${M}.txt ]]; then
-			[[ -f ${TEST}-${M}.txt ]] && mv ${TEST}-${M}.txt{,.old}
-			perf report --stdio --header --percent-limit 0 \
-				-F period,dso,symbol -i ${TEST}-${M}.perf >| ${TEST}-${M}.txt
-		fi
+	if [[ -f ${TEST}.perf ]]; then
+		perf2csv ${TEST}.perf ${TEST}.csv
+		perf2treemap ${TEST}.perf treemap-${TEST}.csv
+		perf script report stackcollapse -i ${TEST}.perf >| ${TEST}.stacks
 
-		if [[ -f ${TEST}-${M}.txt.old ]]; then
-			awk -f ${M}-diff.awk ${TEST}-${M}.txt.old ${TEST}-${M}.txt
+		if [[ ! -f ${TEST}.perf.old ]]; then
+			flamegraph.pl < ${TEST}.stacks > ${TEST}.svg
+		else
+			perf2csv ${TEST}.perf.old ${TEST}.old.csv
+			perf script report stackcollapse -i ${TEST}.perf.old >| ${TEST}.stacks.old
+			diff.pl ${TEST}.stacks{.old,} | flamegraph.pl >| ${TEST}.svg
+			diff.pl ${TEST}.stacks{,.old} | flamegraph.pl >| ${TEST}-inv.svg
+			treemap-diff.sh ${TEST}.perf{.old,} treemap-diff-${TEST}.csv
+		fi
+	fi
+
+	for M in cpu cache; do
+		if [[ ${TEST}-${M}.perf -nt ${TEST}-${M}.csv ]]; then
+			perf2csv ${TEST}-${M}.perf ${TEST}-${M}.csv
+			perf2treemap ${TEST}-${M}.perf treemap-${TEST}-${M}.csv
+		fi
+		if [[ ${TEST}-${M}.perf.old -nt ${TEST}-${M}.old.csv ]]; then
+			perf2csv ${TEST}-${M}.perf.old ${TEST}-${M}.old.csv
+			csv-diff.sh ${TEST}-${M}.perf{.old,} ${TEST}-${M}-diff.csv
 		fi
 	done
-
-	for M in cpu cache; do
-		awk -f ${M}.awk ${TEST}-${M}.txt
-	done
-
-	[[ -f ${TEST}.perf ]] && flamegraph.sh ${TEST}.perf
 done
